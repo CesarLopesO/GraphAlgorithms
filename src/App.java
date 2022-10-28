4915829b042be1b01998e0368d54651f2dbfc024
CesarLopesO <77798696+CesarLopesO@users.noreply.github.com>
2022-10-28 15:20:02 -0300

    Add files via upload

--- src/App.java
+++ src/App.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 class App {
     public static void main(String[] args) throws IOException {
         String arquivos[] = new String[6];
-        
         arquivos[0] = "files/cm/toy.txt";
         arquivos[1] = "files/cm/rg300_4730.txt";
         arquivos[2] = "files/cm/rome99c.txt";
@@ -14,79 +13,86 @@ class App {
 
         Scanner sc = new Scanner(System.in);
 
-        int buton = 0;
-    
+        int button = 0;
 
-        switch (buton) {
-            case 1:
-                System.out.println("Escolha o arquivo para testar:");
-                System.out.println("0 - toy.txt");
-                System.out.println("1 - rg300_4730.txt");
-                System.out.println("2 - rome99c.txt");
-                System.out.println("3 - facebook_combined.txt");
-                System.out.println("4 - USA-road-dt.DC.txt");
-                System.out.println("5 - USA-road-dt.NY.txt");
-                int file = sc.nextInt();
+        while (button != 3) {
+            System.out.println();
+            System.out.println("Informe a tarefa:");
+            System.out.println("1- Caminho Minimo");
+            System.out.println("2- Labirinto");
+            System.out.println("3- Sair");
+            button = sc.nextInt();
 
-                System.out.println("Insira a origem:");
-                int s = sc.nextInt();
-                System.out.println("Insira o destino:");
-                int d = sc.nextInt();
+            switch (button) {
+                case 1:
+                    System.out.println("Escolha o arquivo para testar:");
+                    System.out.println("0 - toy.txt");
+                    System.out.println("1 - rg300_4730.txt");
+                    System.out.println("2 - rome99c.txt");
+                    System.out.println("3 - facebook_combined.txt");
+                    System.out.println("4 - USA-road-dt.DC.txt");
+                    System.out.println("5 - USA-road-dt.NY.txt");
+                    int file = sc.nextInt();
 
-                GraphList g1 = new GraphList(arquivos[file]);
-                GraphMatrix g2 = new GraphMatrix(arquivos[file]);
+                    System.out.println("Insira a origem:");
+                    int s = sc.nextInt();
+                    System.out.println("Insira o destino:");
+                    int d = sc.nextInt();
 
-                System.out.println();
-                System.out.println("[Dijkstra]");
-                System.out.println();
-                long startTime = System.currentTimeMillis();
-                g1.dijkstra(s, d);
-                float totalTime = System.currentTimeMillis() - startTime;
-                System.out.println("O tempo total foi de " + totalTime / 1000 + " segundos.");
+                    GraphList g1 = new GraphList(arquivos[file]);
+                    GraphMatrix g2 = new GraphMatrix(arquivos[file]);
 
-                System.out.println();
-                System.out.println("[Bellman Ford]");
-                System.out.println();
-                startTime = System.currentTimeMillis();
-                g1.bellmanFord(s, d);
-                totalTime = System.currentTimeMillis() - startTime;
-                System.out.println("O tempo total foi de " + totalTime / 1000 + " segundos.");
+                    System.out.println();
+                    System.out.println("[Dijkstra]");
+                    System.out.println();
+                    long startTime = System.currentTimeMillis();
+                    g1.dijkstra(s, d);
+                    float totalTime = System.currentTimeMillis() - startTime;
+                    System.out.println("O tempo total foi de " + totalTime / 1000 + " segundos.");
 
-                System.out.println();
-                System.out.println("[Bellman Ford Melhorado]");
-                System.out.println();
-                startTime = System.currentTimeMillis();
-                g1.bellmanFordMelhorado(s, d);
-                totalTime = System.currentTimeMillis() - startTime;
-                System.out.println("O tempo total foi de " + totalTime / 1000 + " segundos.");
+                    System.out.println();
+                    System.out.println("[Bellman Ford]");
+                    System.out.println();
+                    startTime = System.currentTimeMillis();
+                    g1.bellmanFord(s, d);
+                    totalTime = System.currentTimeMillis() - startTime;
+                    System.out.println("O tempo total foi de " + totalTime / 1000 + " segundos.");
 
-                System.out.println();
-                System.out.println("[Floyd Warshall]");
-                System.out.println();
-                startTime = System.currentTimeMillis();
-                g2.floydWarshall(s, d);
-                totalTime = System.currentTimeMillis() - startTime;
-                System.out.println("O tempo total foi de " + totalTime / 1000 + " segundos.");
-                break;
+                    System.out.println();
+                    System.out.println("[Bellman Ford Melhorado]");
+                    System.out.println();
+                    startTime = System.currentTimeMillis();
+                    g1.bellmanFordMelhorado(s, d);
+                    totalTime = System.currentTimeMillis() - startTime;
+                    System.out.println("O tempo total foi de " + totalTime / 1000 + " segundos.");
 
-            case 2:
-                System.out.println("Informe o nome do arquivo:(Exemplo: files/maze/toy.txt)");
-                String filename = sc.next();
-                long startTimeLabirinto = System.currentTimeMillis();
-                Labirinto labirinto = new Labirinto(filename);
-                float totalTimeLabirinto = System.currentTimeMillis() - startTimeLabirinto;
-                System.out.println("O tempo total foi de " + totalTimeLabirinto / 1000 + " segundos.");
+                    System.out.println();
+                    System.out.println("[Floyd Warshall]");
+                    System.out.println();
+                    startTime = System.currentTimeMillis();
+                    g2.floydWarshall(s, d);
+                    totalTime = System.currentTimeMillis() - startTime;
+                    System.out.println("O tempo total foi de " + totalTime / 1000 + " segundos.");
+                    break;
 
-                break;
-            case 3:
-                break;
+                case 2:
+                    System.out.println("Informe o nome do arquivo:(Exemplo: files/maze/toy.txt)");
+                    String filename = sc.next();
+                    long startTimeLabirinto = System.currentTimeMillis();
+                    Labirinto labirinto = new Labirinto(filename);
+                    float totalTimeLabirinto = System.currentTimeMillis() - startTimeLabirinto;
+                    System.out.println("O tempo total foi de " + totalTimeLabirinto / 1000 + " segundos.");
 
-            default:
-                System.out.println("Opcao invalida!");
+                    break;
+                case 3:
+                    break;
+
+                default:
+                    System.out.println("Opcao invalida!");
+            }
+
         }
 
+        sc.close();
     }
-
-    sc.close();
-       
 }
